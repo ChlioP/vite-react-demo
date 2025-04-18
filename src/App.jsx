3dd89df1593e67638b0b2493b21af3f10020cf19
ChlioP <chliopham@gmail.com>
2025-04-17 21:38:11 -0700

    Initial commit with Vite React setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1> Hello from Vite + React!</h1>
+      <p>This is a simple project to demonstrate Vite setup.</p>
+    </div>
+  );
+}
+
+export default App;
+
